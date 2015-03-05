9dae56fab3482a3cbcf472c7e83f4a4ce16581cb
Sergey Trofimov <sarg@sarg.org.ru>
2015-03-05 15:11:14 +0300

    slock

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -230,6 +230,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ modkey,           }, "l",      function () awful.util.spawn("slock") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
