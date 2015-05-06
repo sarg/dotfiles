baa6a83110dbee7d4f62c4887bdde1c3f897b62f
Sergey Trofimov <sarg@sarg.org.ru>
2015-05-06 23:39:57 +0300

    set path in xsessionrc

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -45,7 +45,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "rxvt"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
