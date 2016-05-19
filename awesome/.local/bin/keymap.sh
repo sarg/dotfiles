fa6714e2bf68ba915e56c03674d4f90a452217e5
Sergey Trofimov <sarg@sarg.org.ru>
2016-05-19 17:14:01 +0300

    swap control and alt

--- awesome/.local/bin/keymap.sh
+++ awesome/.local/bin/keymap.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-setxkbmap us,ru -option grp:alt_shift_toggle
-xmodmap -e 'clear mod1'
-xmodmap -e 'clear control'
-xmodmap -e 'add mod1 = Control_L' -e 'add mod1 = Control_R'
-xmodmap -e 'add Control = Alt_L' -e 'add Control = Alt_R'
+setxkbmap us,ru -option grp:ctrl_shift_toggle
+xmodmap ~/.Xmodmap
