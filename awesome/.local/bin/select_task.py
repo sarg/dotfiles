88ef4e0685e6955cc96b362543fc6070a797a5d0
Sergey Trofimov <sarg@sarg.org.ru>
2016-05-20 15:11:21 +0300

    use tasklib

--- awesome/.local/bin/select_task.py
+++ awesome/.local/bin/select_task.py
@@ -1,12 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import subprocess
 import re
 import sys
 import json
 import pprint
+from tasklib import TaskWarrior
+from tasklib.task import Task
+from subprocess import *
 
-nextTask = subprocess.check_output('task | head -n -2 | tail -n +4 | dmenu -l 15', shell=True)
-nextTask = nextTask.strip()
+
+tw = TaskWarrior(data_location = '~/.task')
+
+dmenu = Popen('dmenu -l 15', shell=True, stdin=PIPE, stdout=PIPE)
+pending_list = tw.tasks.pending()
+for i,t in enumerate(pending_list):
+    descr = '%d %s\n' % (i, t['description'].strip())
+    dmenu.stdin.write(descr.encode('UTF-8'))
+
+dmenu.stdin.close()
+dmenu.wait()
+    
+nextTask = dmenu.stdout.read().decode('UTF-8')
 
 if not nextTask:
     sys.exit(0)
@@ -14,16 +28,17 @@ if not nextTask:
 m = re.search('^(\d+)', nextTask)
 
 if m:
-    taskId = int(m.group(1))
+    task = pending_list[int(m.group(1))-1]
 else:
-    out = subprocess.check_output('task add %s' % nextTask, shell=True)
-    taskId = int(re.search('task (\d+)', out).group(1))
+    task = Task(tw, description=nextTask)
+    task.save()
 
-active=json.loads(subprocess.check_output('task +ACTIVE export', shell=True))
+active = tw.tasks.filter('+ACTIVE')
 if active:
-    activeId = active[0]['id']
+    active = active.get()
+    if not active == task:
+        print("stopping")
+        active.stop()
 
-    if activeId != taskId:
-        subprocess.call('task %d stop' % activeId, shell=True)
-
-subprocess.call('task %d start' % taskId, shell=True)
+if not task.active:
+    task.start()
