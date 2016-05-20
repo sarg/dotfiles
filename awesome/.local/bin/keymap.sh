feb204e2d95c263c00c01086ae2aef49171e4565
Sergey Trofimov <sarg@sarg.org.ru>
2016-05-20 17:09:54 +0300

    xmodmap sticky keys

--- awesome/.local/bin/keymap.sh
+++ awesome/.local/bin/keymap.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
 setxkbmap us,ru -option -option grp:ctrl_shift_toggle
-xmodmap ~/.Xmodmap
+
+cat <<EOF | xmodmap -
+clear control
+clear mod1
+clear lock
+
+keycode 66 = Control_L
+keycode 64 = Control_L
+keycode 37 = Alt_L
+keycode 108 = Control_R
+keycode 105 = Alt_R
+
+add control = Control_L Control_R
+add mod1 = Alt_L Alt_R
+EOF
+
+xkbset -bell -feedback sticky -twokey latchlock
+xkbset exp 64 '=sticky' '=twokey' '=latchlock'
