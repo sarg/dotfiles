adda72e4b54202c5134cc6a2f52403719bce42b2
Sergey Trofimov <sarg@sarg.org.ru>
2016-11-04 23:26:54 +0300

    Restore right alt

--- awesome/.local/bin/keymap.sh
+++ awesome/.local/bin/keymap.sh
@@ -13,12 +13,13 @@ keycode 22 = Control_L
 keycode 66 = Control_L
 keycode 64 = Control_L
 keycode 37 = Alt_L
-keycode 108 = Control_R
-keycode 105 = Alt_R
 
 add control = Control_L Control_R
 add mod1 = Alt_L Alt_R
 EOF
+
+# keycode 108 = Control_R
+# keycode 105 = Alt_R
 
 #xkbset -bell -feedback sticky -twokey latchlock
 #xkbset exp 64 '=sticky' '=twokey' '=latchlock'
