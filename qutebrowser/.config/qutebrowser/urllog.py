53810c523bebd4bdb411e35a4effefb745733cde
Sergey Trofimov <sarg@sarg.org.ru>
2018-06-27 14:31:45 +0300

    Update urllog.

--- qutebrowser/.config/qutebrowser/urllog.py
+++ qutebrowser/.config/qutebrowser/urllog.py
@@ -1,52 +1,51 @@
 import csv
 import time
 import os
+from pathlib import Path
 from qutebrowser.utils import objreg
 from qutebrowser.mainwindow.mainwindow import MainWindow
 from qutebrowser.app import Quitter
 from functools import wraps
 
-csv_file = open('~/.events/qutebrowser', 'at', buffering=1, newline='')
-url_log = csv.writer(csv_file, delimiter=';')
+csv_file = open(Path.home() / '.events/qutebrowser', 'at', buffering=1, newline='')
+url_log = csv.writer(csv_file, delimiter=';', lineterminator='\n')
 
 def log_url(url):
     if not url.isEmpty():
-        url_log.writerow([int(time.time()), 'url', url.toString()])
+        url_log.writerow([time.time(), 'url', url.toString()])
 
-def wrap_showEvent(f):
-    @wraps(f)
-    def wrap(win, e):
-        tab = win.tabbed_browser._now_focused
-        if tab:
-            log_url(tab.url())
-            
-        return f(win, e)
-    
-    return wrap
+def before(func, advice):
+    "Registers `before` advice for function"
+    @wraps(func)
+    def call_advice(*args, **kwargs):
+        advice(*args, **kwargs)
+        return func(*args, **kwargs)
+    return call_advice
 
-def wrap_register(f):
-    def on_new_window(win):
-        tabs = win.tabbed_browser
-        tabs.current_tab_changed.connect(lambda tab: log_url(tab.url()))
-        tabs.cur_url_changed.connect(log_url)
+def on_new_window(win):
+    """Registers handler for tab change and for url change of existing tab."""
+    tabs = win.tabbed_browser
+    tabs.current_tab_changed.connect(lambda tab: log_url(tab.url()))
+    tabs.cur_url_changed.connect(log_url)
+        
+def on_start(name, obj, *args, **kwargs):
+    """Catches a moment when qutebrowser is fully started. When it happens - connects
+    a handler for `new_window` signal."""
 
-    @wraps(f)
-    def add_signals(name, obj, *args, **kwargs):
-        if name == 'app':
-            obj.new_window.connect(on_new_window)
-    
-        return f(name, obj, *args, **kwargs)
-    
-    return add_signals
+    if name == 'app':
+        obj.new_window.connect(on_new_window)
 
-def wrap_shutdown(f):
-    @wraps(f)
-    def save_log(*args, **kwargs):
-       csv_file.close() 
-       return f(*args, **kwargs)
-       
-    return save_log
+def on_shutdown(*args, **kwargs):
+    "Cleans resources"
+    csv_file.close() 
 
-objreg.register = wrap_register(objreg.register)
-MainWindow.showEvent = wrap_showEvent(MainWindow.showEvent)
-Quitter.shutdown = wrap_shutdown(Quitter.shutdown)
+def enterEvent(win, e):
+    "When one of qutebrowser windows receives focus - log it's current url"
+    tab = win.tabbed_browser._now_focused
+    if tab: log_url(tab.url())
+
+    super(MainWindow, win).enterEvent(e)
+
+objreg.register = before(objreg.register, on_start)
+MainWindow.enterEvent = enterEvent
+Quitter.shutdown = before(Quitter.shutdown, on_shutdown)
