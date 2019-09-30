dec1f9afd172b5c223ad2054fade508ab2ad1365
Sergey Trofimov <sarg@sarg.org.ru>
2019-09-30 22:48:36 +0200

    Enable :lang data

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -8,7 +8,7 @@
  :emacs       dired electric
  :term        eshell shell term vterm
  :tools       editorconfig eval flycheck (lookup +docsets) magit pdf (pass +auth)
- :lang        clojure hy emacs-lisp (org +dragndrop +ipython +pandoc +present) rest (sh +fish)
+ :lang        clojure data hy emacs-lisp (org +dragndrop +ipython +pandoc +present) rest (sh +fish)
  :app         calendar (telega +ivy) (rss +org)
  :config      (default +bindings +smartparens)
  :sarg        config exwm)
