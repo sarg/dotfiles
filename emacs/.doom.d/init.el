87baa87912b64ebefe12fcea355ae1a9c76531e9
Sergey Trofimov <sarg@sarg.org.ru>
2019-11-06 07:44:51 +0100

    Enable :emacs ibuffer

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -5,7 +5,7 @@
  :completion  company (ivy +fuzzy)
  :ui          nav-flash modeline ophints (popup +all +defaults) pretty-code unicode vc-gutter window-select treemacs
  :editor      (evil +everywhere) fold lispy snippets (format +onsave) multiple-cursors
- :emacs       dired electric vc
+ :emacs       dired electric vc ibuffer
  :term        eshell shell term vterm
  :tools       editorconfig eval flycheck (lookup +docsets) magit pdf (pass +auth) docker lsp
  :lang        (cc +lsp) clojure data hy emacs-lisp (org +dragndrop +ipython +pandoc +present) rest (sh +fish) plantuml
