5e9d7e94eb796b947c0623736014aaba89ed329a
Sergey Trofimov <sarg@sarg.org.ru>
2023-03-10 21:49:47 +0100

    Replace adwaita icon theme with tango

--- guix/home.scm
+++ guix/home.scm
@@ -71,7 +71,7 @@
   '("picom" "xhost" "xkbcomp" "xkbset"
     "xprop" "xrandr" "xset" "xwininfo"
     "xev" "xclip" "xinput"
-    "hicolor-icon-theme" "adwaita-icon-theme"
+    "hicolor-icon-theme" "tango-icon-theme"
     "igt-gpu-tools"                     ; intel graphics tool
     ))
 
