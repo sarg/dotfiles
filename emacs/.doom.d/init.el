5b6d233f2a994ceaa5e48bc6e4129bb4ae854eb2
Sergey Trofimov <sarg@sarg.org.ru>
2023-03-10 19:49:08 +0100

    Enable doom irc module

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -12,6 +12,6 @@
  :lang        clojure data emacs-lisp
  (org +contacts +dragndrop +pandoc +present +hugo +roam2) beancount
  rest sh plantuml (python +pyenv +lsp) (web +html) yaml json
- :app         calendar telega (rss +org) torrent exwm
+ :app         calendar telega (rss +org) torrent exwm irc
  :email       (mu4e +gmail)
  :config      (default +bindings +smartparens) literate)
