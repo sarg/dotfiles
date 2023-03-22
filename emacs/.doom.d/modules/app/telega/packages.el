7b62060b0491651d26cab56be7fd803a92aa929b
Sergey Trofimov <sarg@sarg.org.ru>
2023-03-22 09:49:50 +0100

    bump doom, pin all locally defined packages

--- emacs/.doom.d/modules/app/telega/packages.el
+++ emacs/.doom.d/modules/app/telega/packages.el
@@ -1,2 +1,2 @@
 (package! telega :built-in 'prefer)
-(package! page-break-lines)
+(package! page-break-lines :pin "79eca86e0634ac68af862e15c8a236c37f446dcd")
--- emacs/.doom.d/modules/app/torrent/packages.el
+++ emacs/.doom.d/modules/app/torrent/packages.el
@@ -1,3 +1,3 @@
 ;; -*- no-byte-compile: t; -*-
-(package! torrent-mode :recipe (:host github :repo "sarg/torrent-mode.el"))
-(package! aria2)
+(package! torrent-mode :recipe (:host github :repo "sarg/torrent-mode.el") :pin "61b4d43bfc25342613f2eac6e37ff64e00452fb9")
+(package! aria2 :pin "1f2cbe624f3a4e0109b5dc123bb4bbed496b15a7")
