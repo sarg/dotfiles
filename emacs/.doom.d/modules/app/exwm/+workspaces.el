7707f77e8d9c11b5435c5901eca45be1a72d2054
Sergey Trofimov <sarg@sarg.org.ru>
2023-03-29 09:45:24 +0200

    Start with 2 workspaces.

--- emacs/.doom.d/modules/app/exwm/+workspaces.el
+++ emacs/.doom.d/modules/app/exwm/+workspaces.el
@@ -1,14 +1,5 @@
-(defvar exwm-workspace-names '("code" "brow" "extr" "lisp" ))
-(setq exwm-workspace-number (length exwm-workspace-names))
-(defsubst exwm-workspace-name-to-index (name)
-  (-elem-index name exwm-workspace-names))
-
-(setq exwm-workspace-index-map
-      (lambda (index)
-        (if (< index (length exwm-workspace-names))
-            (elt exwm-workspace-names index)
-          (number-to-string index))))
-
+(setq exwm-workspace-number 2)
+;;; quick switch to last
 ;; Quick switching between workspaces
 (defvar exwm-toggle-workspace 0
   "Previously selected workspace. Used with `exwm-jump-to-last-exwm'.")
@@ -22,6 +13,7 @@
 
 (exwm-input-set-key (kbd "<s-tab>") #'exwm-jump-to-last-exwm)
 
+;;; shortcuts
 ;; + Set shortcuts to switch to a certain workspace.
 ;; use all digits, so that if new workspace created it could be switched to
 (dotimes (i 9)
@@ -30,7 +22,20 @@
                          (interactive)
                          (exwm-workspace-switch ,i))))
 
-;; EWMH integration
+;;; EWMH integration with names support
+(defvar exwm-workspace-names
+  '("code" "brow" "extr" "lisp"))
+
+(defsubst exwm-workspace-name-to-index (name)
+  "Returns workspace index by NAME."
+  (-elem-index name exwm-workspace-names))
+
+(setq exwm-workspace-index-map
+      (lambda (index)
+        (if (< index (length exwm-workspace-names))
+            (elt exwm-workspace-names index)
+          (number-to-string index))))
+
 (defun exwm-workspace--update-ewmh-desktop-names ()
   (xcb:+request exwm--connection
       (make-instance 'xcb:ewmh:set-_NET_DESKTOP_NAMES
