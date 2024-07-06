58e186e77b1325d831b3b92494dc5b4b23afa09c
Sergey Trofimov <sarg@sarg.org.ru>
2024-07-06 13:31:39 +0200

    treesheets and airscan

---
+++ guix/personal/personal/packages/treesheets.scm
@@ -0,0 +1,48 @@
+(define-module (personal packages treesheets)
+  #:use-module (guix download)
+  #:use-module (guix utils)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system)
+  #:use-module (guix build-system cmake)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages wxwidgets)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (guix packages))
+
+(define-public treesheets
+  (package
+   (name "treesheets")
+   (version "9723493654")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/aardappel/treesheets")
+                  (commit version)))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32 "0fkfbivaxq1l72i28cbnz1maffcj631mnbm8j3yqs3s3bk2cr4bk"))))
+   (build-system ;; cmake-build-system
+     ;; remove once cmake-minimal >= 3.25
+     (build-system
+       (name 'cmake-full)
+       (description "full cmake")
+       (lower
+        (lambda* (name #:rest arguments)
+          (apply (build-system-lower cmake-build-system)
+                 name (ensure-keyword-arguments arguments `(#:cmake ,cmake)))))))
+   (arguments
+    '(#:tests? #f))
+   (native-inputs (list pkg-config))
+   (inputs (list wxwidgets))
+   (home-page "https://strlen.com/treesheets")
+   (synopsis "Free form data organiser (hierarchical spreadsheet)")
+   (description "A 'hierarchical spreadsheet' that is a great replacement for
+ spreadsheets, mind mappers, outliners, PIMs, text editors and small databases.
+Suitable for any kind of data organization, such as todo lists, calendars,
+project management, brainstorming, organizing ideas, planning, requirements
+gathering, presentation of information, etc.")
+   (license license:zlib)))
+
+treesheets
