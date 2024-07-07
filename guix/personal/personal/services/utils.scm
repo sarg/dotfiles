f0601c69b414487320950c7490efa8703d013690
Sergey Trofimov <sarg@sarg.org.ru>
2024-07-07 09:18:56 +0200

    augment-computed-file util

--- guix/personal/personal/services/utils.scm
+++ guix/personal/personal/services/utils.scm
@@ -1,11 +1,13 @@
 (define-module (personal services utils)
+  #:use-module (guix gexp)
   #:use-module (gnu services)
   #:use-module (gnu services shepherd)
   #:use-module (ice-9 match)
 
-  #:export (no-autostart))
+  #:export (no-autostart augment-computed-file))
 
 (define (no-autostart input-service)
+  "Augment shepherd extension of INPUT-SERVICE to disable auto-start."
   (define (transform-extension ex)
     (match ex
       (($ (@@ (gnu services) <service-extension>)
@@ -30,3 +32,19 @@
        (inherit kind)
        (extensions (map transform-extension extensions)))
       value))))
+
+(define (augment-computed-file proc content)
+  "Wrap PROC returning a <computed-file> to append CONTENT to that file."
+  (lambda* (#:rest r)
+    (let ((f (apply proc r)))
+      (computed-file
+       (computed-file-name f)
+
+       #~(begin
+           #$(computed-file-gexp f)
+
+           (let ((port (open-file #$output "a")))
+             (format port #$content)
+             (close port)))
+
+       #:options (computed-file-options f)))))
