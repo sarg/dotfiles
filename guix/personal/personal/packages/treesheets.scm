71584a0e6b7fed1528b3579bd32e5452345cd354
Sergey Trofimov <sarg@sarg.org.ru>
2024-07-09 07:05:44 +0200

    simplify treesheets

--- guix/personal/personal/packages/treesheets.scm
+++ guix/personal/personal/packages/treesheets.scm
@@ -13,36 +13,31 @@
 
 (define-public treesheets
   (package
-   (name "treesheets")
-   (version "9723493654")
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "https://github.com/aardappel/treesheets")
-                  (commit version)))
-            (file-name (git-file-name name version))
-            (sha256
-             (base32 "0fkfbivaxq1l72i28cbnz1maffcj631mnbm8j3yqs3s3bk2cr4bk"))))
-   (build-system ;; cmake-build-system
-     ;; remove once cmake-minimal >= 3.25
-     (build-system
-       (name 'cmake-full)
-       (description "full cmake")
-       (lower
-        (lambda* (name #:rest arguments)
-          (apply (build-system-lower cmake-build-system)
-                 name (ensure-keyword-arguments arguments `(#:cmake ,cmake)))))))
-   (arguments
-    '(#:tests? #f))
-   (native-inputs (list pkg-config))
-   (inputs (list wxwidgets))
-   (home-page "https://strlen.com/treesheets")
-   (synopsis "Free form data organiser (hierarchical spreadsheet)")
-   (description "A 'hierarchical spreadsheet' that is a great replacement for
+    (name "treesheets")
+    (version "9723493654")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/aardappel/treesheets")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32 "0fkfbivaxq1l72i28cbnz1maffcj631mnbm8j3yqs3s3bk2cr4bk"))
+              (modules '((guix build utils)))
+              (snippet
+               '(for-each delete-file-recursively
+                 (list "lib" "osx" "treesheets")))))
+    (build-system cmake-build-system)
+    (arguments `(#:tests? #f #:cmake ,cmake))
+    (native-inputs (list pkg-config))
+    (inputs (list wxwidgets))
+    (home-page "https://strlen.com/treesheets")
+    (synopsis "Free form data organiser (hierarchical spreadsheet)")
+    (description "A 'hierarchical spreadsheet' that is a great replacement for
  spreadsheets, mind mappers, outliners, PIMs, text editors and small databases.
 Suitable for any kind of data organization, such as todo lists, calendars,
 project management, brainstorming, organizing ideas, planning, requirements
 gathering, presentation of information, etc.")
-   (license license:zlib)))
+    (license license:zlib)))
 
 treesheets
