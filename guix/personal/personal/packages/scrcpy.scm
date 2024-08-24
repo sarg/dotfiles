68dd2c3ad43b6ef891dc7a81f48c6fa277d65e98
Sergey Trofimov <sarg@sarg.org.ru>
2024-08-24 22:52:59 +0200

    Bump scrcpy.

--- guix/personal/personal/packages/scrcpy.scm
+++ guix/personal/personal/packages/scrcpy.scm
@@ -1,5 +1,6 @@
 (define-module (personal packages scrcpy)
   #:use-module (guix download)
+  #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix build-system meson)
   #:use-module ((guix licenses) #:prefix license:)
@@ -11,21 +12,10 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (guix packages))
 
-(define scrcpy-server
-  (let ((version "2.5"))
-    (origin
-     (method url-fetch)
-     (uri (string-append "https://github.com/Genymobile/scrcpy"
-                         "/releases/download/v" version "/"
-                         "scrcpy-server-v" version))
-     (sha256
-      (base32
-       "05gbjq9hjb5vqvcs9mvxhvp0dsiarl8gcsx2fd457zvabl8b320l")))))
-
 (define-public scrcpy
   (package
    (name "scrcpy")
-   (version "2.5")
+   (version "2.6.1")
    (source (origin
             (method git-fetch)
             (uri (git-reference
@@ -33,21 +23,30 @@
                   (commit (string-append "v" version))))
             (file-name (git-file-name name version))
             (sha256
-             (base32 "1c815ddac7x50xrl016601vy8bv3wkp73ps4h31qnnb5ggdmkmjm"))))
+             (base32 "1hfslc1pc1h8cl41n4v9pcijbi2s7m32jk3bdszaj4c95qm914x7"))))
    (build-system meson-build-system)
-   (native-inputs (list pkg-config))
-   (inputs (list ffmpeg sdl2 libusb scrcpy-server adb))
+   (native-inputs (list pkg-config
+                        (origin
+                          (method url-fetch)
+                          (uri (string-append "https://github.com/Genymobile/scrcpy"
+                                              "/releases/download/v" version "/"
+                                              "scrcpy-server-v" version))
+                          (file-name (string-append "scrcpy-server-" version ".jar"))
+                          (sha256
+                           (base32
+                            "0fywx2zsbfy1w7i5rrh8p2jkz635wf1h7hwrfnpyb8155q5vayna")))))
+   (inputs (list ffmpeg sdl2 libusb adb))
    (arguments
-    `(#:configure-flags (list "-Dcompile_server=false")
-      #:phases (modify-phases %standard-phases
-                              (add-after 'install 'wrap-executable-with-adb-path
-                                         (lambda* (#:key inputs outputs #:allow-other-keys)
-                                           (let* ((out (assoc-ref outputs "out"))
-                                                  (jar (assoc-ref inputs "_"))
-                                                  (adb (assoc-ref inputs "adb")))
-                                             (wrap-program (string-append out "/bin/scrcpy")
-                                               `("ADB" = (,(string-append adb "/bin/adb")))
-                                               `("SCRCPY_SERVER_PATH" = (,jar)))))))))
+    (list
+     #:configure-flags #~(list "-Dcompile_server=false")
+     #:phases #~(modify-phases %standard-phases
+                  (add-after 'install 'wrap-executable-with-adb-path
+                    (lambda* (#:key inputs outputs #:allow-other-keys)
+                      (let* ((jar (assoc-ref inputs "_"))
+                             (adb (search-input-file inputs "/bin/adb")))
+                        (wrap-program (string-append #$output "/bin/scrcpy")
+                          `("ADB" = (,adb))
+                          `("SCRCPY_SERVER_PATH" = (,jar)))))))))
    (home-page "https://github.com/Genymobile/scrcpy")
    (synopsis "Display and control your Android device")
    (description "Scrcpy (screen copy) provides display and control of Android devices connected
