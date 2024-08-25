51716e31a844985a60c49bb0504f3a7d7869abb9
Sergey Trofimov <sarg@sarg.org.ru>
2024-08-25 15:58:11 +0200

    Add mumi with xdg-utils replaced by jaro.

---
+++ guix/personal/personal/packages/mumi.scm
@@ -0,0 +1,15 @@
+(define-module (personal packages mumi)
+  #:use-module (personal packages jaro)
+  #:use-module (guix utils)
+  #:use-module (gnu packages mail)
+  #:use-module (guix packages))
+
+(define-public mumi-with-jaro
+  (package
+    (inherit mumi)
+    (name "mumi-with-jaro")
+    (inputs
+     (modify-inputs (package-inputs mumi)
+       (replace "xdg-utils" jaro)))))
+
+mumi-with-jaro
