95e9456381b7588e69e2468d78b484817963ee6b
Sergey Trofimov <sarg@sarg.org.ru>
2024-09-14 10:19:30 +0200

    Reconfigure.

--- guix/channels.scm
+++ guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "b46256b162e15420bb034a9e6d65ec46f1c03343")
+          "2cf45e9ef113b8a5d7a0e4c102b368fc15e8161c")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -14,7 +14,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "6b54040cb44afeaad6ba52f054b40d00056393e7")
+          "b8d6f9b9137dd89b399944f04977101a1929ad5e")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -25,4 +25,4 @@
         (url "https://github.com/sarg/dotfiles")
         (branch "master")
         (commit
-          "b87483e8ed6223973d8de00e73957843880bd86f")))
+          "2cbef5ec7660eba3f8f1b8591214b493020d8c75")))
