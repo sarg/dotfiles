e1100b655dba2fb00a5548fa1176abffcd74483e
Sergey Trofimov <sarg@sarg.org.ru>
2024-12-11 22:49:44 +0100

    Scrcpy 3.1

--- guix/personal/personal/packages/scrcpy.scm
+++ guix/personal/personal/packages/scrcpy.scm
@@ -15,7 +15,7 @@
 (define-public scrcpy
   (package
    (name "scrcpy")
-   (version "2.7")
+   (version "3.1")
    (source (origin
             (method git-fetch)
             (uri (git-reference
@@ -23,7 +23,7 @@
                   (commit (string-append "v" version))))
             (file-name (git-file-name name version))
             (sha256
-             (base32 "0gksv13dpxni8qm1qk47m1a8bkp0v1mynmyz4mddc892d7y2c4rq"))))
+             (base32 "0janx96lv15673yfn3zys1x5i70463icmxg3qcgdkapgbp0fa6sz"))))
    (build-system meson-build-system)
    (native-inputs (list pkg-config
                         (origin
@@ -34,7 +34,7 @@
                           (file-name (string-append "scrcpy-server-" version ".jar"))
                           (sha256
                            (base32
-                            "1fmdl5vi7l36vagalyqf0wkgmzzanfy2fb82bh80y9kcydcmcg52")))))
+                            "1h465fr3rlhniirml5yahaw086jchhafps8n7brv28rglr20k3wm")))))
    (inputs (list ffmpeg sdl2 libusb adb))
    (arguments
     (list
