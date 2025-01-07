741c5208e4dc2dd247fcb069fd028d8db91ec284
Sergey Trofimov <sarg@sarg.org.ru>
2025-01-07 22:02:08 +0100

    Add latest restic

--- guix/personal/personal/packages/binary.scm
+++ guix/personal/personal/packages/binary.scm
@@ -67,3 +67,31 @@
    (synopsis "babashka clojure scripting runtime")
    (description "babashka clojure scripting runtime")
    (license license:epl1.0)))
+
+(define-public restic
+  (let* ((version "0.17.3")
+         (pkg (string-append "restic_" version "_linux_amd64")))
+    (package
+      (name "restic")
+      (version version)
+      (source (origin
+                (method url-fetch)
+                (uri (string-append
+                      "https://github.com/restic/restic/releases/download/v"
+                      version "/" pkg ".bz2"))
+                (sha256
+                 (base32 "1lyl69336y2n06zrpn7y863pyqrnlvdyydkfmrx1c4xalvnzm5sh"))))
+      (build-system binary-build-system)
+      (supported-systems '("x86_64-linux"))
+      (arguments (list
+                  #:install-plan
+                  #~(list '(#$pkg "bin/restic"))
+                  #:phases
+                  #~(modify-phases %standard-phases
+                      (add-before 'install 'chmod
+                        (lambda _
+                          (chmod #$pkg #o555))))))
+      (home-page "https://restic.net")
+      (synopsis "backup software")
+      (description "backup software")
+      (license license:bsd-2))))
