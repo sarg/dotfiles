6a8eeea2f22ef5472ab1f5b7e113e954c32eb8d6
Sergey Trofimov <sarg@sarg.org.ru>
2025-04-01 11:44:26 +0200

    Check in custom.el

---
+++ emacs/.doom.d/custom.el
@@ -0,0 +1,23 @@
+(put 'erase-buffer 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "/storage/data/emacs/bookmarks")
+ '(custom-safe-themes t)
+ '(cyclekey-languages '("German"))
+ '(safe-local-variable-directories
+   '("/storage/devel/ext/rde/" "/home/sarg/.local/doom/straight/repos/magit/"
+     "/storage/devel/ext/nonguix/" "/storage/devel/ext/guix/"
+     "/storage/devel/ext/sops-guix/" "/home/sarg/.dotfiles/emacs/.doom.d/"
+     "/gnu/store/4d2xd3rkl06lrqf58qgs479gn5sljkrc-doomemacs-3.0.0-20250331.67933f9/"))
+ '(safe-local-variable-values '((eval sarg/eval-org-src-block "api"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'list-timers 'disabled nil)
