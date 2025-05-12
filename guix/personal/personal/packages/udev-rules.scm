9c30fc98345f452a7078845f386ead68da77c924
Sergey Trofimov <sarg@sarg.org.ru>
2025-05-12 08:55:55 +0200

    Add udev rules for thinkpad niceties

--- guix/personal/personal/packages/udev-rules.scm
+++ guix/personal/personal/packages/udev-rules.scm
@@ -70,3 +70,53 @@
     (description "udev rules for UBSNet pixel")
     (license license:unlicense)
     (home-page #f)))
+
+(define-public iwlwifi-led-udev-rules
+  (package
+   (name "iwlwifi-led-udev-rules")
+   (version "1")
+   (source #f)
+   (build-system trivial-build-system)
+   (arguments
+    `(#:modules ((guix build utils))
+      #:builder
+      (begin
+        (use-modules (guix build utils))
+        (let* ((out (assoc-ref %outputs "out"))
+               (rules (string-append out "/lib/udev/rules.d")))
+          (mkdir-p rules)
+          (call-with-output-file
+              (string-append rules "/98-iwlwifi-led.rules")
+            (lambda (port)
+              (format port
+                      "ACTION==~s, SUBSYSTEM==~s, KERNEL==~s, ATTR{trigger}=~s"
+                      "add" "leds" "phy0-led" "phy0radio")))))))
+   (synopsis "udev rules for iwlwifi led")
+   (description "udev rules for iwlwifi led")
+   (license license:unlicense)
+   (home-page #f)))
+
+(define-public backlight-udev-rules
+  (package
+   (name "backlight-udev-rules")
+   (version "1")
+   (source #f)
+   (build-system trivial-build-system)
+   (arguments
+    `(#:modules ((guix build utils))
+      #:builder
+      (begin
+        (use-modules (guix build utils))
+        (let* ((out (assoc-ref %outputs "out"))
+               (rules (string-append out "/lib/udev/rules.d")))
+          (mkdir-p rules)
+          (call-with-output-file
+              (string-append rules "/98-backlight.rules")
+            (lambda (port)
+              (format port
+                      "ACTION==~s, SUBSYSTEM==~s, ATTR{brightness}=~s"
+                      "add" "backlight" "$attr{max_brightness}")))))))
+   (synopsis "udev rules to set backlight brightness to max")
+   (description "udev rules to set backlight brightness to max")
+   (license license:unlicense)
+   (home-page #f)))
