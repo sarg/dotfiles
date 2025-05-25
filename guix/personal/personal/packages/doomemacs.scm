7c5bce7f1a1fce84f65559f5ebcc434f21eda2dd
Sergey Trofimov <sarg@sarg.org.ru>
2025-05-25 15:39:30 +0200

    Bump doom

--- guix/personal/personal/packages/doomemacs.scm
+++ guix/personal/personal/packages/doomemacs.scm
@@ -7,8 +7,8 @@
   #:use-module (guix packages))
 
 (define-public doomemacs
-  (let ((commit "11b4b8d2e5072f1f62df27943ca378c616678a72")
-        (revision "20250518"))
+  (let ((commit "8406c1ff22b95bd0f816de4a0223fa3ce3c82568")
+        (revision "20250525"))
     (package
       (name "doomemacs")
       (version (git-version "3.0.0" revision commit))
@@ -20,7 +20,7 @@
                (commit commit)))
          (file-name (git-file-name "doomemacs" version))
          (sha256
-          (base32 "01x6d3nxk41ajjbd181dqf4dy44fraxg5185ihz209c5dyanxnp8"))))
+          (base32 "0x6b9r5rd5fx24y4wxm4wqip4g2gr7ck7177di02a4cbv4wj1sdc"))))
 
       (build-system copy-build-system)
       (home-page "https://github.com/doomemacs/doomemacs")
