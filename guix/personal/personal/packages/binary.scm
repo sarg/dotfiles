353832de2837fbec632ccedb6a5ea88c05964ad8
Sergey Trofimov <sarg@sarg.org.ru>
2025-08-06 12:17:16 +0200

    packages: add jujutsu

--- guix/personal/personal/packages/binary.scm
+++ guix/personal/personal/packages/binary.scm
@@ -427,3 +427,23 @@ command-line programs gsutil and gcloud among others.")
    (synopsis #f)
    (description #f)
    (license license:expat)))
+
+(define-public jujutsu
+  (package
+   (name "jujutsu")
+   (version "0.31.0")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (string-append
+           "https://github.com/jj-vcs/jj/releases/download/v" version "/jj-v" version "-x86_64-unknown-linux-musl.tar.gz"))
+     (sha256
+      (base32 "0487k053kl9ifz4bx19af3as6zr612r1ifr0skl9cw31x30xh5xj"))))
+   (build-system copy-build-system)
+   (supported-systems '("x86_64-linux"))
+   (arguments
+    `(#:install-plan '(("jj" "bin/"))))
+   (home-page "https://github.com/jj-vcs/jj")
+   (synopsis #f)
+   (description #f)
+   (license license:asl2.0)))
