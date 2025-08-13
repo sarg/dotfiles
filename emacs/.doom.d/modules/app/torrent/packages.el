32be3167e9654f9fe9652e57374225b40b8b188b
Sergey Trofimov <sarg@sarg.org.ru>
2025-08-13 17:35:52 +0200

    config: bump torrent-mode

--- emacs/.doom.d/modules/app/torrent/packages.el
+++ emacs/.doom.d/modules/app/torrent/packages.el
@@ -1,3 +1,3 @@
 ;; -*- no-byte-compile: t; -*-
-(package! torrent-mode :pin "c7ef935dcf7b999981f441794ddbd98c3216bfee")
+(package! torrent-mode :pin "5dbb59c60c0c2db24d3d138eb003c66c3578b7b4")
 (package! aria2 :pin "1f2cbe624f3a4e0109b5dc123bb4bbed496b15a7")
