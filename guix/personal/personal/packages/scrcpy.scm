eadb6c68da88663ab232ac13f00ef0d299f23f96
Sergey Trofimov <sarg@sarg.org.ru>
2025-12-18 13:30:18 +0100

    packages: update scrcpy

--- guix/personal/personal/packages/scrcpy.scm
+++ guix/personal/personal/packages/scrcpy.scm
@@ -10,12 +10,33 @@
   #:use-module (gnu packages libusb)
   #:use-module (gnu packages android)
   #:use-module (gnu packages pkg-config)
-  #:use-module (guix packages))
+  #:use-module (guix packages)
+  #:use-module (guix build-system copy))
+
+(define-public scrcpy-server
+  (package
+   (name "scrcpy-server")
+   (version "3.3.4")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "https://github.com/Genymobile/scrcpy"
+                                "/releases/download/v" version "/" name
+                                "-v" version))
+            (file-name "scrcpy-server.jar")
+            (sha256
+             (base32
+              "17h70cimwdhfpkhzdl5mp7zxjhamdmzfrdh655aal02ska627245"))))
+   (build-system copy-build-system)
+   (home-page "https://github.com/Genymobile/scrcpy")
+   (synopsis "Server component for scrcpy")
+   (description "This package provides the scrcpy-server.jar file needed by scrcpy
+to communicate with Android devices.")
+   (license license:asl2.0)))
 
 (define-public scrcpy
   (package
    (name "scrcpy")
-   (version "3.3.3")
+   (version "3.3.4")
    (source (origin
             (method git-fetch)
             (uri (git-reference
@@ -23,18 +44,9 @@
                   (commit (string-append "v" version))))
             (file-name (git-file-name name version))
             (sha256
-             (base32 "1520my5d1385s91kra89bk92ksgn6343lk4zjq5gkngmpylq0h6n"))))
+             (base32 "1gn75sqqv0r0zh67rqfqmg5il7ac3lkd2wyy0ji9y7s9fmh7h9z7"))))
    (build-system meson-build-system)
-   (native-inputs (list pkg-config
-                        (origin
-                          (method url-fetch)
-                          (uri (string-append "https://github.com/Genymobile/scrcpy"
-                                              "/releases/download/v" version "/"
-                                              "scrcpy-server-v" version))
-                          (file-name (string-append "scrcpy-server-" version ".jar"))
-                          (sha256
-                           (base32
-                            "1q2b6pakyqdywzi92v9c5c8fifpg9yn9gkmcsjfn8ngjlwxk4w3y")))))
+   (native-inputs (list pkg-config scrcpy-server))
    (inputs (list ffmpeg sdl2 libusb adb))
    (arguments
     (list
@@ -42,7 +54,7 @@
      #:phases #~(modify-phases %standard-phases
                   (add-after 'install 'wrap-executable-with-adb-path
                     (lambda* (#:key inputs #:allow-other-keys)
-                      (let* ((jar (assoc-ref inputs #$(format #f "scrcpy-server-~a.jar" version)))
+                      (let* ((jar (search-input-file inputs "/scrcpy-server.jar"))
                              (adb (search-input-file inputs "/bin/adb")))
                         (wrap-program (string-append #$output "/bin/scrcpy")
                           `("ADB" = (,adb))
