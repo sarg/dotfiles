230c71ae26963d1ece32eddfd85de219175ae868
Sergey Trofimov <sarg@sarg.org.ru>
2026-01-29 08:55:44 +0100

    config: add user-dirs.dirs, clean up torrent module

--- emacs/.doom.d/modules/app/torrent/config.el
+++ emacs/.doom.d/modules/app/torrent/config.el
@@ -1,56 +1,23 @@
 ;;; -*- lexical-binding: t; -*-
 
-
-(defvar aria2-notify-completion-timer nil
-  "Timer object for `aria2-notify-completion-mode` to check aria2 status.")
-
-(defun aria2-notify-completion-check ()
-  (when (or (not aria2--cc)
-            (and
-             (seq-empty-p (tellActive aria2--cc aria2--tell-keys))
-             (seq-empty-p (tellWaiting aria2--cc nil nil aria2--tell-keys))))
-    
-    (setq mode-line-process '(:propertize ":🏁" face gnus-summary-high-read))
-    (notifications-notify :title "aria2" :body "All downloads complete")
-    (cancel-timer aria2-notify-completion-timer)))
-
-(define-minor-mode aria2-notify-completion-mode
-  "Notify when aria2 downloads complete."
-  :global nil
-  :group 'aria2
-  :ligher "🏁"
-  
-  (cond
-   ((not (and (eq major-mode 'aria2-mode)
-              aria2--cc))
-    (error "Choose aria2 buffer first."))
-   
-   (aria2-notify-completion-mode
-    (setq mode-line-process '(:propertize ":⚐" face gnus-summary-high-read))
-    (setq aria2-notify-completion-timer
-          (run-with-timer 0 10 #'aria2-notify-completion-check)))
-
-   (t
-    (when aria2-notify-completion-timer
-      (cancel-timer aria2-notify-completion-timer)
-      (setq mode-line-process nil)
-      (setq aria2-notify-completion-timer nil)))))
-
-
-(defun aria2-format-select-file (files)
-  (mapconcat (lambda (idx) (number-to-string (1+ idx))) files ","))
-
 (defun aria2-download (fn &rest selected)
   (require 'aria2)
   (let ((dir (read-directory-name "Directory: " aria2-download-directory nil t)))
     (aria2-downloads-list)
     (addTorrent aria2--cc fn
-                :select-file (aria2-format-select-file selected)
+                :select-file (mapconcat (lambda (idx) (number-to-string (1+ idx)))
+                                        selected ",")
                 :dir (expand-file-name dir))))
 
 (use-package! torrent-mode
   :custom
   (torrent-mode-download-function #'aria2-download))
+
+(defun aria2-notify-completion (entry)
+  (notifications-notify
+   :title "Download complete"
+   :app-name "aria2"
+   :body (aria2--list-entries-File entry)))
 
 (use-package! aria2
   :defer t
@@ -58,18 +25,23 @@
   (set-popup-rule! aria2-list-buffer-name :ignore t)
 
   ;; upstream it
-  (evil-collection-inhibit-insert-state 'aria2-mode-map)
-  (evil-set-initial-state 'aria2-mode-map 'normal)
-  (evil-collection-define-key 'normal 'aria2-mode-map
-    "Q" 'aria2-terminate
-    "p" 'aria2-toggle-pause
-    "a" 'aria2-add
-    "D" 'aria2-remove-download
-    "C" 'aria2-clean-removed-download
-    (kbd "M-k") 'aria2-move-up-in-list
-    (kbd "M-j") 'aria2-move-down-in-list)
+  (when (modulep! :editor evil +everywhere)
+    (evil-collection-inhibit-insert-state 'aria2-mode-map)
+    (evil-set-initial-state 'aria2-mode-map 'normal)
+    (evil-collection-define-key 'normal 'aria2-mode-map
+      "Q" 'aria2-terminate
+      "p" 'aria2-toggle-pause
+      "a" 'aria2-add
+      "D" 'aria2-remove-download
+      "C" 'aria2-clean-removed-download
+      (kbd "M-k") 'aria2-move-up-in-list
+      (kbd "M-j") 'aria2-move-down-in-list))
+
+  :hook
+  (aria2-on-complete . aria2-notify-completion)
 
   :custom
-  (aria2-download-directory (expand-file-name "~/Downloads"))
+  (aria2-download-directory (or (xdg-user-dir "DOWNLOAD")
+                                (expand-file-name "~")))
   (aria2-start-rpc-server t)
   (aria2-custom-args '("--rpc-save-upload-metadata=false")))
--- emacs/.doom.d/modules/app/torrent/packages.el
+++ emacs/.doom.d/modules/app/torrent/packages.el
@@ -1,5 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
-(package! torrent-mode :built-in 'prefer
-  :pin "5dbb59c60c0c2db24d3d138eb003c66c3578b7b4")
-(package! aria2 :built-in 'prefer
-  :pin "1f2cbe624f3a4e0109b5dc123bb4bbed496b15a7")
+(package! torrent-mode :pin "6a3d09e8c5c75b127bfc22048593d6ee2cfeb7be")
+(package! aria2 :recipe (:host github :repo "sarg/aria2")
+  :pin "0e9ea273f3bb09bb117a9593d548b88763e4cdd4")
