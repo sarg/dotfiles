370b8a09fe243058d8fdea4355bc60c2e30b4e0d
Sergey Trofimov <sarg@sarg.org.ru>
2026-07-28 07:24:52 +0200

    reconfigure

--- guix/personal/personal/packages/emacs-xyz.scm
+++ guix/personal/personal/packages/emacs-xyz.scm
@@ -1451,28 +1451,6 @@ calendar-holidays holiday-german-RP-holidays) This works for for all states:
 https://github.com/abo-abo/netherlands-holidays.")
     (license #f)))
 
-(define-public emacs-just-mode
-  (package
-    (name "emacs-just-mode")
-    (version "0.1.9")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/leon-barrett/just-mode.el")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1czf779akdcx72ma7x9v70kjbic73312fi1czbzvlvxr01pjpyj0"))))
-    (build-system emacs-build-system)
-    (arguments (list #:tests? #f)) ; no tests
-    (home-page "https://github.com/leon-barrett/just-mode.el")
-    (synopsis "Justfile editing mode")
-    (description
-     "This package provides a major mode for editing justfiles, as defined by the tool
-\"just\" (@url{https://github.com/casey/just}).")
-    (license license:gpl3+)))
-
 (define-public emacs-justl
   (package
     (name "emacs-justl")
